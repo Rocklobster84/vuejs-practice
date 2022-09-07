ae1bd65a41149571e5db79078100e968773db738
Rocklobster84 <thompson.stephanie84@gmail.com>
2022-09-07 14:17:01 -0500

    Complete first assignment

--- basics-assignment-1-problem/app.js
+++ basics-assignment-1-problem/app.js
@@ -1,12 +1,19 @@
 const app = Vue.createApp({
  data() {
   return {
-
+    myName: 'Steph',
+    myAge: 38,
+    miscImg: 'https://cdn.pixabay.com/photo/2015/04/23/21/59/tree-736877_1280.jpg',
   }
  },
  methods: {
-
+  calcAge() {
+   return this.myAge + 5;
+  },
+  calcRando() {
+   return Math.random;
+  }
  }
 });
 
-app.mount();
+app.mount('#assignment');
